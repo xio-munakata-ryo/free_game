d4c29ba9dc8b710662d2e55537fa65bdf2a0be58
mr <a@a>
2025-06-30 13:12:13 +0900

    [Add] TestGame

---
+++ kiki/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-PhotonTest-1.0";
+const contentToCache = [
+    "Build/20250630forGit.loader.js",
+    "Build/20250630forGit.framework.js.unityweb",
+    "Build/20250630forGit.data.unityweb",
+    "Build/20250630forGit.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
